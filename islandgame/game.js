87dd9c64445a83b201526a397a4055359408a5e0
roshinyyy <158239354+roshinyyy@users.noreply.github.com>
2025-11-18 13:57:58 +0800

    Add files via upload

---
+++ islandgame/game.js
@@ -0,0 +1,19 @@
+﻿// --- now the game config ---
+var config = {
+  type: Phaser.AUTO,
+  width: 32 * 20,
+  height: 32 * 20,
+  physics: {
+    default: "arcade",
+    arcade: { debug: true },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  backgroundColor: "#4315a7ff",
+  pixelArt: true,
+  scene: [world, room1,], 
+};
+
+var game = new Phaser.Game(config);
